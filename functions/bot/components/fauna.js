bc80ef9f197a1aa93098b50d550f9ebb10ae7111
Simon Mayerhofer <git@maysi.de>
2020-01-14 00:33:03 +0100

    Add functions to set and get options

--- functions/bot/components/fauna.js
+++ functions/bot/components/fauna.js
@@ -52,3 +52,98 @@ exports.newUser = async user => {
 			});
 	});
 };
+
+async function optionExists(key) {
+	console.log(`* Check if option "${key}" exists.`);
+
+	return new Promise((res, rej) => {
+		client
+			.query(
+				q.Map(
+					q.Paginate(q.Match(q.Index('options_search_by_key'), key)),
+					q.Lambda('X', q.Get(q.Var('X'))),
+				),
+			)
+			.then(ret => {
+				console.log(`* Option ${ret.data.length === 0 ? 'not ' : ''}found.`);
+				res(ret.data.length !== 0);
+			});
+	});
+}
+
+exports.setOption = async (key, value) => {
+	console.log(`Set option "${key}".`);
+	const exists = await optionExists(key);
+
+	if (exists) {
+		console.log('* key exists. update key.');
+		return new Promise((res, rej) => {
+			client
+				.query(
+					q.Update(
+						q.Select(
+							'ref',
+							q.Get(q.Match(q.Index('options_search_by_key'), key)),
+						),
+						{
+							data: {
+								key,
+								value,
+							},
+						},
+					),
+				)
+				.then(ret => {
+					console.log('* key updated.');
+					res(ret);
+				})
+				.catch(err => {
+					console.log('* update failed.');
+					rej(err);
+				});
+		});
+	}
+
+	return new Promise((res, rej) => {
+		client
+			.query(
+				q.Create(q.Collection('options'), {
+					data: {
+						key,
+						value,
+					},
+				}),
+			)
+			.then(ret => {
+				console.log('* new key set.');
+				res(ret);
+			})
+			.catch(err => {
+				console.log('* new key set failed.');
+				rej(err);
+			});
+	});
+};
+
+exports.getOption = async (key, defaultValue) => {
+	console.log(`Get option by key: "${key}"`);
+
+	return new Promise((res, rej) => {
+		client
+			.query(
+				q.Map(
+					q.Paginate(q.Match(q.Index('options_search_by_key'), key)),
+					q.Lambda('X', q.Get(q.Var('X'))),
+				),
+			)
+			.then(ret => {
+				if (ret.data.length === 0) {
+					console.log(`* No option found. Return default value.`);
+					res(defaultValue);
+				}
+
+				console.log(`* Option found.`);
+				res(ret.data[0].data.value);
+			});
+	});
+};
