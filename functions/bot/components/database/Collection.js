ded5652e73b0d99629b34e8bc0ae643275b955df
Simon Mayerhofer <git@maysi.de>
2020-01-14 23:46:49 +0100

    Create all index on collection creation

--- functions/bot/components/database/Collection.js
+++ functions/bot/components/database/Collection.js
@@ -11,6 +11,13 @@ class Collection {
 		return this.name;
 	}
 
+	/**
+	 * Returns name of all index.
+	 */
+	getAllIndexName() {
+		return `${this.name}_all`;
+	}
+
 	async createCollection() {
 		console.log(`\nCreate collection ${this.getName()}`);
 
@@ -55,13 +62,68 @@ class Collection {
 		});
 	}
 
+	async createAllIndex() {
+		console.log(`* create index ${this.getAllIndexName()}`);
+
+		const { client, q } = this;
+
+		return new Promise((res, rej) => {
+			client
+				.query(
+					q.CreateIndex({
+						name: `${this.getAllIndexName()}`,
+						source: q.Collection(this.getName()),
+						serialized: true,
+					}),
+				)
+				.then(() => {
+					console.log('* successfully created');
+					res();
+				})
+				.catch(err => {
+					rej(err);
+				});
+		});
+	}
+
+	async allIndexExists() {
+		console.log(`* check if index ${this.getAllIndexName()} exists.`);
+
+		const { client, q } = this;
+
+		return new Promise((res, rej) => {
+			client
+				.query(q.Paginate(q.Indexes()))
+				.then(ret => {
+					let exists = false;
+					ret.data.forEach(index => {
+						if (index.id === `${this.getAllIndexName()}`) {
+							exists = true;
+						}
+					});
+					console.log(
+						`* index ${exists ? 'already exists' : 'does not exist'}.`,
+					);
+					res(exists);
+				})
+				.catch(err => {
+					console.log(err);
+					rej(err);
+				});
+		});
+	}
+
 	async setup() {
 		console.log(`\nSetup collection ${this.getName()}`);
 
-		const exists = await this.collectionExists();
-
-		if (!exists) {
+		const collExists = await this.collectionExists();
+		if (!collExists) {
 			await this.createCollection();
+		}
+
+		const allIndexExists = await this.allIndexExists();
+		if (!allIndexExists) {
+			await this.createAllIndex();
 		}
 	}
 }
