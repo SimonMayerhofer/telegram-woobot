4b27dad0104bcfdb3d55c4d1f8b9481d75759de2
Simon Mayerhofer <git@maysi.de>
2020-01-15 00:38:30 +0100

    Add search by id/key indicies

--- functions/bot/components/database/OptionsCollection.js
+++ functions/bot/components/database/OptionsCollection.js
@@ -5,6 +5,30 @@ class OptionsCollection extends Collection {
 		super(client, query);
 		this.name = 'options';
 	}
+
+	/**
+	 * Returns name of search by key index.
+	 */
+	getSearchByKeyIndexName() {
+		return `${this.name}_search_by_key`;
+	}
+
+	async setup() {
+		await super.setup();
+
+		const searchByKeyIndexExists = await this.indexExists(
+			this.getSearchByKeyIndexName(),
+		);
+		if (!searchByKeyIndexExists) {
+			await this.createIndex({
+				name: `${this.getSearchByKeyIndexName()}`,
+				source: this.q.Collection(this.getName()),
+				unique: true,
+				serialized: true,
+				terms: [{ field: ['data', 'key'] }],
+			});
+		}
+	}
 }
 
 exports.OptionsCollection = OptionsCollection;
--- functions/bot/components/database/UsersCollection.js
+++ functions/bot/components/database/UsersCollection.js
@@ -5,6 +5,30 @@ class UsersCollection extends Collection {
 		super(client, query);
 		this.name = 'users';
 	}
+
+	/**
+	 * Returns name of search by id index.
+	 */
+	getSearchByIdIndexName() {
+		return `${this.name}_search_by_id`;
+	}
+
+	async setup() {
+		await super.setup();
+
+		const searchByIdIndexExists = await this.indexExists(
+			this.getSearchByIdIndexName(),
+		);
+		if (!searchByIdIndexExists) {
+			await this.createIndex({
+				name: `${this.getSearchByIdIndexName()}`,
+				source: this.q.Collection(this.getName()),
+				unique: true,
+				serialized: true,
+				terms: [{ field: ['data', 'id'] }],
+			});
+		}
+	}
 }
 
 exports.UsersCollection = UsersCollection;
